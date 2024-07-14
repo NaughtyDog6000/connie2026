c621502902556e53d4c0837b84fc7853309aa739
nd6k <nd6k@nd6k.uk>
2024-07-14 22:44:23 +0100

    rsvp page submit form via fetch/json

--- src/routes/RSVPage.tsx
+++ src/routes/RSVPage.tsx
@@ -14,6 +14,27 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 export default function RSVPContent() {
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const name = formData.get("name")
+        const email = formData.get("email")
+        const attendees_str = formData.get("attendees")
+        const attendees: number = Number(attendees_str)
+        console.log({ name, email, attendees })
+
+        await fetch("https://connie-api.nd6k.uk/rsvp", {
+            method: "POST",
+            mode: "no-cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+
+            body: JSON.stringify({ name, email, attendees }),
+        });
+    }
+
+
     return (
         <div className="mx-auto max-w-[600px] space-y-8 py-12 px-4 md:px-0">
             <div className="space-y-2 text-center">
@@ -22,20 +43,20 @@ export default function RSVPContent() {
                     Let us know that you will be in attendance.
                 </p>
             </div>
-            <form className="space-y-4">
+            <form className="space-y-4" onSubmit={handleSubmit}>
                 <div className="grid grid-cols-2 gap-4">
                     <div className="space-y-2">
                         <Label htmlFor="name">Name</Label>
-                        <Input id="name" placeholder="John Doe" required />
+                        <Input id="name" name="name" placeholder="John Doe" required />
                     </div>
                     <div className="space-y-2">
                         <Label htmlFor="email">Email</Label>
-                        <Input id="email" type="email" placeholder="john@example.com" required />
+                        <Input id="email" name="email" type="email" placeholder="john@example.com" required />
                     </div>
                 </div>
                 <div className="space-y-2">
                     <Label htmlFor="attendees">Number of Attendees</Label>
-                    <Input id="attendees" type="number" placeholder="2" min="1" max="10" required />
+                    <Input id="attendees" name="attendees" type="number" placeholder="2" min="1" max="10" required />
                 </div>
                 <Button type="submit" className="w-full">
                     RSVP
